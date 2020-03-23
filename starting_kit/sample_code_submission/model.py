ecf7dc752ce5f4b8685785df8a4dca568b4d8225
LouiseALLAIN <59994656+LouiseALLAIN@users.noreply.github.com>
2020-03-23 20:10:50 +0100

    Add files via upload

--- starting_kit/sample_code_submission/model.py
+++ starting_kit/sample_code_submission/model.py
@@ -12,72 +12,39 @@ import numpy as np   # We recommend to use numpy arrays
 from os.path import isfile
 from sklearn.base import BaseEstimator
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import make_scorer
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import ParameterGrid
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+import numpy as np
+from sklearn.model_selection import ShuffleSplit
 
 class model (BaseEstimator):
-   def __init__(self, classifier=RandomForestClassifier(random_state=42)):
-   
-        '''
-        This constructor is supposed to initialize data members.
-        Use triple quotes for function documentation. 
-         args :
-            classifier : classifier we will use for making our predictions
-        '''
-        self.num_train_samples=0
-        self.num_feat=1
-        self.num_labels=1
-        self.is_trained=False
-        self.classifier = classifier
+   def __init__(self):
+    self.classifier = RandomForestClassifier(max_depth=100, n_estimators=100, random_state=30)
         
         
-   def fit(self, X, y):
-        '''
-        This function should train the model parameters.
-        Here we do nothing in this example...
-        Args:
-            X: Training data matrix of dim num_train_samples * num_feat.
-            y: Training label matrix of dim num_train_samples * num_labels.
-        Both inputs are numpy arrays.
-        
-        '''
-        
-        
-        self.num_train_samples = X.shape[0]
-        if X.ndim>1: self.num_feat = X.shape[1]
-        print("FIT: dim(X)= [{:d}, {:d}]".format(self.num_train_samples, self.num_feat))
-        num_train_samples = y.shape[0]
-        if y.ndim>1: self.num_labels = y.shape[1]
-        print("FIT: dim(y)= [{:d}, {:d}]".format(num_train_samples, self.num_labels))
-        if (self.num_train_samples != num_train_samples):
-            print("ARRGH: number of samples in X and y do not match!")
-        
-        
-        
-        self.classifier.fit(X, np.ravel(y))
-        self.is_trained=True
-        
-        return self
+   def fit(self, X, y):   
+         self.classifier.fit(X, np.ravel(y))
+         return self
    
    def predict(self, X):
-        '''
-        This function  provides predictions of labels on (test) data
-        '''
-        num_test_samples = X.shape[0]
-        if X.ndim>1: num_feat = X.shape[1]
-        print("PREDICT: dim(X)= [{:d}, {:d}]".format(num_test_samples, num_feat))
-        if (self.num_feat != num_feat):
-            print("ARRGH: number of features in X does not match training data!")
-        print("PREDICT: dim(y)= [{:d}, {:d}]".format(num_test_samples, self.num_labels))
-        
-        
         return self.classifier.predict(X)
+
    def save(self, path="./"):
-      pickle.dump(self, open(path + '_model.pickle', "wb"))
+        pickle.dump(self, open(path + '_model.pickle', "wb"))
 
    def load(self, path="./"):
-      modelfile = path + '_model.pickle'
-      if isfile(modelfile):
-         with open(modelfile, 'rb') as f:
-            self = pickle.load(f)
-         print("Model reloaded from: " + modelfile)
-      return self
-
+        modelfile = path + '_model.pickle'
+        if isfile(modelfile):
+            with open(modelfile, 'rb') as f:
+                self = pickle.load(f)
+        print("Model reloaded from: " + modelfile)
+        return self
